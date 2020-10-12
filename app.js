65e07fc6fa6088c381804c6a776d9209c3280a84
Daniel2711 <detaddeo.d@gmail.com>
2020-10-12 18:13:11 +0200

    Struttura js con prompt per nome

---
+++ app.js
@@ -0,0 +1,8 @@
+var name = "name";
+var cognome = "cognome"
+var colore = "colore";
+
+namePrompt = prompt("Come ti chiami?", "nome inserito");
+var spanName = document.getElementById("nome");
+spanName.innerHTML = namePrompt;
+console.log(name);
