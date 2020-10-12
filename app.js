9ba1c192bd640cab787f2ed93326895a524d169a
Daniel2711 <detaddeo.d@gmail.com>
2020-10-12 19:30:57 +0200

    Card cognome con effetto

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ var name = "name";
 var cognome = "cognome"
 var colore = "colore";
 
+// nome
 name = prompt("Come ti chiami?", "nome inserito");
 var spanName = document.getElementById("nome");
 spanName.innerHTML = name;
 console.log(name);
 
+// cognome
 cognome = prompt("Quale è il tuo cognome?", "cognome inserito");
 var spanCognome = document.getElementById("cognome");
 spanCognome.innerHTML = cognome;
