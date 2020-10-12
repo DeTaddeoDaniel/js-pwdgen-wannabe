b76ab29afff2ecca9d92a89a982f87b1b9dd2fb4
Daniel2711 <detaddeo.d@gmail.com>
2020-10-12 18:59:38 +0200

    Card cognome inserita

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ var name = "name";
 var cognome = "cognome"
 var colore = "colore";
 
-namePrompt = prompt("Come ti chiami?", "nome inserito");
+name = prompt("Come ti chiami?", "nome inserito");
 var spanName = document.getElementById("nome");
-spanName.innerHTML = namePrompt;
+spanName.innerHTML = name;
 console.log(name);
+
+cognome = prompt("Quale è il tuo cognome?", "cognome inserito");
+var spanCognome = document.getElementById("cognome");
+spanCognome.innerHTML = cognome;
+console.log(cognome);
