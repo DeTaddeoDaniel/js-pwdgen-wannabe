92ae6ec20d0b5d8a158e73ac584e0f4bd489958f
Daniel2711 <detaddeo.d@gmail.com>
2020-10-15 11:12:02 +0200

    Comment in english

--- Versione2/app.js
+++ Versione2/app.js
@@ -1,31 +1,31 @@
 var name = "user";
-var cognome = "cognome"
+var surname = "surname"
 var color = "color";
 var password = "";
 
-// nome
+// name
 name = prompt("What is your name?");
 console.log("name: " + name);
 
-// cognome
-cognome = prompt("What is your surname?");
-console.log ("surname: " + cognome)
+// surname
+surname = prompt("What is your surname?");
+console.log ("surname: " + surname)
 
-// colore
+// color
 color = prompt("What is like color?");
 console.log("color: " + color);
 
-// password generata
-password = name + cognome + color + "20";
+// generate password
+password = name + surname + color + "20";
 console.log("password: " + password);
 
-// inserimento della pagina
+// add element page
 var elementPassword = document.getElementById("passwordGenerate");
 elementPassword.innerHTML = password;
 
 var elementUser = document.getElementById("helloUser");
 elementUser.innerHTML = "Hi " + name + "!" ;
 
-// animazione
+// animation
 var animation = document.getElementById('seeAnimation');
 console.log(animation);
